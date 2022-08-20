d347fe183c2f9e72c2f3830330457a84131b27f6
Lauren Main <lauren.main28@gmail.com>
2022-08-20 16:12:38 -0700

    adds componentdidmount

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      errorMessage: ''
     }
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount = async () => {
+    try {
+      const config = {
+        method: 'get', // get is default behavior
+        baseURL: process.env.REACT_APP_SERVER,
+        url: '/books'
+      };
+
+      const response = await axios(config);
+      console.log(response.data);
+      this.setState({ cats: response.data });
+    } catch(error) {
+      console.error('Error is in the componentDidMount Function: ', error);
+      // axios sends more info about the error in a response object on the error
+      this.setState({ errorMessage: `Status Code ${error.response.status}: ${error.response.data}`});
+    }
+  }
 
   render() {
 
